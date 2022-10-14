de6ca39a70e7d365707b8245b99fd091734c594e
ador <rashedulhaque111@gmail.com>
2022-10-14 22:42:25 +0600

    design update on charts

--- src/Components/Chart.jsx
+++ src/Components/Chart.jsx
@@ -32,82 +32,82 @@ const Chart = () => {
     const lineData = [
         {
             "name": "Jan",
-            "Consumer Goods": 400,
-            "Gadget": 240,
-            "amt": 240
+            "Consumer Goods": 200,
+            "Gadget": 140,
+            "Others": 90
         },
         {
             "name": "Feb",
-            "Consumer Goods": 300,
-            "Gadget": 180,
-            "amt": 220
+            "Consumer Goods": 500,
+            "Gadget": 380,
+            "Others": 320
         },
         {
             "name": "Mar",
-            "Consumer Goods": 200,
+            "Consumer Goods": 600,
             "Gadget": 680,
-            "amt": 290
+            "Others": 690
         },
         {
             "name": "Apr",
-            "Consumer Goods": 270,
-            "Gadget": 398,
-            "amt": 200
+            "Consumer Goods": 470,
+            "Gadget": 728,
+            "Others": 600
         },
         {
             "name": "May",
-            "Consumer Goods": 190,
-            "Gadget": 480,
-            "amt": 211
+            "Consumer Goods": 490,
+            "Gadget": 580,
+            "Others": 311
         },
         {
             "name": "Jun",
-            "Consumer Goods": 230,
-            "Gadget": 380,
-            "amt": 200
+            "Consumer Goods": 730,
+            "Gadget": 440,
+            "Others": 415
         },
         {
             "name": "Jul",
-            "Consumer Goods": 390,
+            "Consumer Goods": 750,
             "Gadget": 430,
-            "amt": 210
+            "Others": 390
         },
         {
             "name": "Aug",
-            "Consumer Goods": 390,
-            "Gadget": 430,
-            "amt": 200
+            "Consumer Goods": 790,
+            "Gadget": 530,
+            "Others": 400
         },
         {
             "name": "Sep",
-            "Consumer Goods": 349,
-            "Gadget": 400,
-            "amt": 210
+            "Consumer Goods": 829,
+            "Gadget": 600,
+            "Others": 410
         },
         {
             "name": "Oct",
-            "Consumer Goods": 390,
-            "Gadget": 430,
-            "amt": 210
+            "Consumer Goods": 470,
+            "Gadget": 630,
+            "Others": 710
         },
         {
             "name": "Nov",
-            "Consumer Goods": 340,
-            "Gadget": 400,
-            "amt": 210
+            "Consumer Goods": 510,
+            "Gadget": 670,
+            "Others": 750
         },
         {
             "name": "Dec",
-            "Consumer Goods": 490,
-            "Gadget": 300,
-            "amt": 200
+            "Consumer Goods": 950,
+            "Gadget": 690,
+            "Others": 600
         }
     ]
     return (
-        <div className='ml-96 grid lg:grid-cols-2 sm:grid-cols-1'>
+        <div className='ml-96 flex flex-row ...'>
             <div>
                 <h1 className='m-8 text-xl text-bold'>Daily Visits</h1>
-                <BarChart width={480} height={250} data={data}>
+                <BarChart width={380} height={250} data={data}>
                     <CartesianGrid strokeDasharray="3 3" />
                     <XAxis dataKey="name" />
                     <YAxis />
@@ -120,7 +120,7 @@ const Chart = () => {
 
             <div>
                 <h1 className='m-8 text-xl text-bold'>Income</h1>
-                <LineChart width={480} height={250} data={lineData}
+                <LineChart width={590} height={250} data={lineData}
                     margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
                     <CartesianGrid strokeDasharray="3 3" />
                     <XAxis dataKey="name" />
@@ -129,6 +129,7 @@ const Chart = () => {
                     <Legend />
                     <Line type="monotone" dataKey="Consumer Goods" stroke="#8C5FF6" />
                     <Line type="monotone" dataKey="Gadget" stroke="#D946EF" />
+                    <Line type="monotone" dataKey="Others" stroke="#F97316" />
                 </LineChart>
             </div>
 
---
+++ src/Components/User.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const User = () => {
+    return (
+
+        <div class="w-full bg-gray-200 rounded-full ml-96">
+            <div class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style="width: 45%"> 45%</div>
+        </div>
+
+    );
+};
+
+export default User;
